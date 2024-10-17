ee5cf3655411fbb41185ce1091f7b32f6efd981f
Jan Stratil <jan.stratil@innovatrics.com>
2024-10-17 15:51:15 +0200

    Resolve the locale

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# Set locale
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
 # Install all necessary packages
 apt update
 apt install -y stow git git-lfs zsh curl wget tmux
+
+# Generate the en_US.UTF-8 locale
+locale-gen en_US.UTF-8
+
+# Reconfigure locales
+dpkg-reconfigure --frontend=noninteractive locales
 
 # Install oh-my-zsh
 rm -rf ~/.oh-my-zsh
